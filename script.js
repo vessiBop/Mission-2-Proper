140f2f0e4ec499f33de74855d0ca173f9fe51a32
Silvestre Fernandez <rasa356@outlook.com>
2022-09-12 13:36:52 +1200

    starting repo of mission 2

---
+++ script.js
@@ -0,0 +1,36 @@
+let navbarElement = document.getElementById('navbar');
+// let currentColourIndex = 0;
+// let buttons = document.getElementsByClassName('buttons');
+
+const themeButton = document.getElementById('changeThemeButton');
+const bodyStyle = document.getElementById('body');
+function changeColour() {
+    if (bodyStyle.style.background === 'white') {
+        bodyStyle.style.background = 'gray';
+    } else {
+        bodyStyle.style.background = 'white';
+    }
+}
+
+let themeBtn = themeButton.addEventListener('click', (event) => {
+    console.log(`button has been clicked`);
+    changeColour();
+    changeBodyTextColour();
+})
+
+function changeBodyTextColour() {
+    if (bodyStyle.style.color === 'black') {
+        bodyStyle.style.color = 'blanchedalmond';
+    } else {
+        bodyStyle.style.color = 'black';
+    }
+}
+
+function changeThemeText() {
+    if (document.getElementById('theme-txt').innerText === 'Light Theme') {
+        document.getElementById('theme-txt').innerText = 'Light Theme';
+    } else {
+        document.getElementById('theme-txt').innerText = 'Dark Theme';
+    }
+}
+
